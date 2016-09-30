6ccfd9554d2996e8c003043d7853f81da99bdb9e
Amin sheikhi <aminsheikhi@gmail.com>
2016-09-30 18:38:18 +0330

    second commit for test cd

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res){
-    res.send('Hello accounting');
+    res.send('<h1>Hello for second commint</h1>');
 });
+
+app.get('/contact', function(req,res){
+    res.send('<h1>Contact</h1>');
+});
+
 
 app.listen(process.env.PORT || 1001, function () {
     console.log('port is listening');
