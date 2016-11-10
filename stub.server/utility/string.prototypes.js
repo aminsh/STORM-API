413d350abfdbdb26aeffad56eacb1165e88abefd
Amin sheikhi <aminsheikhi@gmail.com>
2016-11-10 22:09:28 +0330

    stub server removed

--- stub.server/utility/string.prototypes.js
+++
@@ -1,9 +0,0 @@
-String.prototype.format = function () {
-    var s = this,
-        i = arguments.length;
-
-    while (i--) {
-        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
-    }
-    return s;
-};
