b07c53de97a31e7d10f8cad44454a9fc0b2c86b0
amin <aminsheikhi@gmail.com>
2017-04-09 16:06:07 +0430

    moved period and mode section to header

--- accounting/client/src/directives/shell.header.js
+++ accounting/client/src/directives/shell.header.js
@@ -1,10 +1,22 @@
-export default function () {
+export default function (fiscalPeriodApi , currentService) {
     return {
         restrict: 'E',
         templateUrl: 'partials/templates/shell.header-template.html',
         replace: true,
         link: (scope, element, attrs) => {
+            scope.fiscalPeriods = [];
+
             scope.toggleSidebar = () =>  scope.$emit('toggle-sidebar');
+
+            fiscalPeriodApi.getAll()
+                .then(result => scope.fiscalPeriods = result.data);
+
+            scope.currentFiscalPeriod = currentService.get().fiscalPeriod;
+
+            scope.selectFiscalPeriod = current => {
+                currentService.setFiscalPeriod(current.id);
+                scope.currentFiscalPeriod = current.id;
+            };
         }
     }
 }
