faf71b3bbab0c53f60c8cf6443cc2d71d3b71065
amin <aminsheikhi@gmail.com>
2017-04-17 19:39:58 +0430

    added test env

--- storm/server/config/index.js
+++ storm/server/config/index.js
@@ -43,6 +43,41 @@ const path = require('path'),
             env
 
         },
+        test: {
+            rootPath: rootPath,
+            db: {
+                client: 'pg',
+                connection: process.env.DATABASE_URL
+            },
+            port: process.env.PORT,
+            version: {
+                vendor: '1.0.0',
+                acc: '1.0.0',
+                css: '1.0.0',
+                template: '1.0.0'
+            },
+            email: {
+                from: 'STORM <info@storm-online.ir>',
+                transporter: {
+                    host: 'smtp.zoho.com',
+                    port: 465, //example
+                    auth: {
+                        user: 'info@storm-online.ir',
+                        pass: 'rAEMtxezr3UN'
+                    }
+                }
+            },
+            url: {
+                luca: 'https://storm-sandbox.herokuapp.com/luca-demo'
+            },
+            auth: {
+                google: {
+                    clientID: '44908669153-rgtap5scj693g240t9p3k69tplearpto.apps.googleusercontent.com',
+                    clientSecret: 'ZAc3SYGLyKenCssgRzs0iY-1',
+                    callbackURL: 'https://storm-sandbox.herokuapp.com/auth/google/callback',
+                }
+            }
+        },
         production: {
             rootPath: rootPath,
             db: {
