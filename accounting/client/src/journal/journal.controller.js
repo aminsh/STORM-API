f7c2fbe1231d1f8ce5173a3a741c98956fd8eb7e
amin <aminsheikhi@gmail.com>
2017-05-09 19:20:05 +0430

    optimized journalQuery performance

--- accounting/client/src/journal/journal.controller.js
+++ accounting/client/src/journal/journal.controller.js
@@ -124,7 +124,18 @@ export default class {
             this.journalApi.getById(this.id)
                 .then(result => {
                     this.journal = result;
+
+                    result.journalLines.forEach(line => {
+                        let subsidiaryLedgerAccount = this.subsidiaryLedgerAccount.data
+                            .asEnumerable().single(s => s.id == line.subsidiaryLedgerAccountId);
+
+                        line.hasDetailAccount = subsidiaryLedgerAccount.hasDetailAccount;
+                        line.hasDimension1 = subsidiaryLedgerAccount.hasDimension1;
+                        line.hasDimension2 = subsidiaryLedgerAccount.hasDimension2;
+                        line.hasDimension3 = subsidiaryLedgerAccount.hasDimension3;
+                    });
                     this.journalLines = result.journalLines;
+
                     this.$scope.$broadcast('grid-changed');
                 })
                 .finally(() => this.isJournalLineLoading = false);
@@ -183,7 +194,7 @@ export default class {
 
         this.journalApi.update(this.id, this.journal)
             .then(() => logger.success())
-            .catch(err =>{
+            .catch(err => {
                 console.log(err);
                 errors = err;
             })
