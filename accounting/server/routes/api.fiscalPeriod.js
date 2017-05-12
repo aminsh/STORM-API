ad2ee4f6065d7728c7a8b40b548d7b13e7c4e0d5
ryped <rypedram@gmail.com>
2017-05-12 16:28:34 +0430

    Implemented "Create FiscalPeriod For The First Time" ٪Refactor٪

--- accounting/server/routes/api.fiscalPeriod.js
+++ accounting/server/routes/api.fiscalPeriod.js
@@ -3,6 +3,7 @@
 const async = require('asyncawait/async'),
     await = require('asyncawait/await'),
     router = require('express').Router(),
+    translate = require('../services/translateService'),
     FiscalPeriodRepository = require('../data/repository.fiscalPeriod'),
     FiscalPeriodQuery = require('../queries/query.fiscalPeriod');
 
@@ -11,6 +12,34 @@ router.route('/')
         let fiscalPeriodQuery = new FiscalPeriodQuery(req.cookies['branch-id']),
             result = await(fiscalPeriodQuery.getAll(req.query));
         res.json(result);
-    }));
+    }))
+    .post(async((req, res) => {
+        let fiscalPeriodRepository = new FiscalPeriodRepository(req.cookies['branch-id']),
+            cmd = req.body,
+            errors = [],
+            entity = {
+                minDate: cmd.minDate,
+                maxDate: cmd.maxDate,
+                isClosed: false
+            };
+
+        if (entity.minDate > entity.maxDate) {
+            errors.push(translate('From date can not be greater than to date'));
+           return res.json({
+                errors,
+                isValid: false
+            });
+        }
+
+        entity = await(fiscalPeriodRepository.create(entity));
+
+        res.cookie('current-period', entity.id);
+
+        res.json({
+            isValid: true
+        });
+
+    }))
+
 
 module.exports = router;
