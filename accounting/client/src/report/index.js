b81e8092df5e791489b7d149613d9953e4d0c644
amin <aminsheikhi@gmail.com>
2017-05-28 20:28:26 +0430

    removed stimulsoft file from first loading

--- accounting/client/src/report/index.js
+++ accounting/client/src/report/index.js
@@ -6,10 +6,12 @@ import ReportListController from './reportList';
 import { ReportParametersController, reportParametersModal } from './reportParameters';
 import ReportPrintController from './reportPrint';
 import ReportDesignController from'./reportDesign';
+import ReportLoaderService from './reportLoaderService';
 
 appModule
     .controller('reportListController', ReportListController)
     .controller('reportParameterController', ReportParametersController)
     .controller('ReportPrintController', ReportPrintController)
     .controller('reportDesignController', ReportDesignController)
+    .service('reportLoaderService', ReportLoaderService)
     .factory('reportParameters', reportParametersModal);
