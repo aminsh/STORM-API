074e8850068c10a191f08e899be3ac882cf38499
Sanaz Soleimani <sanaz.slm1988@gmail.com>
2017-06-05 23:39:03 +0430

    add invoice report/query

--- accounting/client/src/apis/reportApi.js
+++ accounting/client/src/apis/reportApi.js
@@ -90,4 +90,9 @@ export default class {
     detailSubsidiaryDetailJournal(params) {
         return this.apiPromise.get(`${this.urlPrefix}/detail-subsidiary-detail-journal`,params);
     }
+
+    invoice(invoiceId){
+        return this.apiPromise.get(`${this.urlPrefix}/journal-lines/${invoiceId}`);
+    }
+
 }
