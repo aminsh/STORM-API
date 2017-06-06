181347f451aaf09293e5b13387f81f51d3e8c028
Sanaz Soleimani <sanaz.slm1988@gmail.com>
2017-06-06 17:20:09 +0430

    ***

--- accounting/server/queries/query.report.invoices.js
+++ accounting/server/queries/query.report.invoices.js
@@ -20,13 +20,13 @@ module.exports = class InvoicesQuery extends BaseQuery {
         "invoiceLines".quantity as quantity, "invoiceLines"."unitPrice" as "unitPrice",
         "invoiceLines".vat as vat,"invoiceLines".discount as discount,
         (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount) as "grossPrice",
-        (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount)*0.09 as "vatPrice",
+        (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount)* ("invoiceLines".vat*0.01) as "vatPrice",
         (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount) 
-            + (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount)*0.09 as "netPrice",
+            + (("invoiceLines"."unitPrice" * "invoiceLines".quantity)-"invoiceLines".discount)*("invoiceLines".vat*0.01) as "netPrice",
         "invoiceLines".description as "invoiceLineDescription",
         "products".title as "productName",
         "detailAccounts".title as "customerName", "detailAccounts".address as "customerAddress",
-        CASE WHEN "detailAccounts"."personType" = 'legal' THEN "detailAccounts"."economyCode" 
+        CASE WHEN "detailAccounts"."personType" = 'legal' THEN "detailAccounts"."economicCode" 
              WHEN "detailAccounts"."personType" = 'real' THEN "detailAccounts"."nationalCode" END as "personCode"`))
             .from('invoices')
             .where('invoices.branchId', branchId)
