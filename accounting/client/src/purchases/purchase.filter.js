dff697cfd9506fe8cdddf2e2e903ad0abfd176c2
ryped <rypedram@gmail.com>
2017-06-06 17:46:58 +0430

    invoices refactor

--- accounting/client/src/purchases/purchase.filter.js
+++
@@ -1,7 +0,0 @@
-export  function totalPrice () {
-    return (lines) => lines.asEnumerable().sum(item => item.quantity* item.unitPrice);
-}
-
-export  function sumTotalPrice () {
-    return (lines) => lines.asEnumerable().sum(item => (item.unitPrice *item.quantity)-(item.tax+item.vat+item.discount));
-}
--- accounting/client/src/sales/salesInvoice.filter.js
+++ accounting/client/src/sales/salesInvoice.filter.js
@@ -2,7 +2,7 @@ export  function totalPrice () {
     return (lines) => lines.asEnumerable().sum(item => item.quantity* item.unitPrice);
 }
 export  function sumTotalPrice () {
-    return (lines) => lines.asEnumerable().sum(item => ((item.unitPrice * item.quantity)-item.discount) + ((item.unitPrice * item.quantity)-item.discount)*(item.tax/100));
+    return (lines) => lines.asEnumerable().sum(item =>((item.unitPrice * item.quantity)-item.discount) + ((item.unitPrice * item.quantity)-item.discount)*(item.tax/100));
 }
 export  function sumTotalTaxPrice () {
     return (lines) => lines.asEnumerable().sum(item => ((item.unitPrice * item.quantity)-item.discount)*(item.tax/100));
