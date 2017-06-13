f4cbefd06782340c024417f5703e539f01a8152a
ryped <rypedram@gmail.com>
2017-06-13 16:43:47 +0430

    bank implemented

---
+++ accounting/client/src/bank/bankListController.js
@@ -0,0 +1,88 @@
+export default class peopleListController {
+    constructor(translate,
+                devConstants,
+                navigate,
+                logger,
+                $timeout,
+                bankApi,
+                confirm,
+                $scope) {
+
+        this.$scope = $scope;
+        this.navigate=navigate;
+        this.$timeout = $timeout;
+        this.logger = logger;
+        this.translate = translate;
+        this.bankApi = bankApi;
+        this.errors = [];
+        $scope.gridOption = {
+            dataSource: new kendo.data.DataSource({
+                serverFiltering: true,
+                serverPaging: true,
+                pageSize: 20,
+                transport: {
+                    read: {
+                        url: devConstants.urls.bank.getAll(),
+                        dataType: "json"
+                    }
+                },
+                schema: {
+                    data: 'data',
+                    total: 'total'
+                }
+            }),
+            toolbar: ["excel"],
+            excel: {
+                fileName: "export.xls",
+                filterable: true
+            },
+            reorderable: true,
+            resizable: true,
+            sortable: true,
+            scrollable: {
+                virtual: true
+            },
+            filterable: true,
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                buttonCount: 10
+            },
+            columns: [
+                {
+                    field: "title", title: translate('title'), width: '120px',
+                    filterable: {
+                        multi: true,
+                        cell: {
+                            operator: "contains",
+                            suggestionOperator: "contains"
+                        }
+                    }
+                },
+                { command: [
+                    { text: translate('Remove'), click:function (e) {
+                        e.preventDefault();
+                        let bank = this.dataItem($(e.currentTarget).closest("tr"));
+                        confirm(
+                            translate('Remove Person'),
+                            translate('Are you sure ?'))
+                            .then(function () {
+                                bankApi.remove(bank.id)
+                                    .then(function () {
+                                        logger.success();
+                                        $scope.gridOption.refresh();
+                                    })
+                                    .catch((errors) => $scope.errors = errors)
+                                    .finally(() => $scope.isSaving = false);
+                            })
+
+                    }},
+                    { text: translate('Edit'), click:function (e) {
+                        e.preventDefault();
+                        let bank = this.dataItem($(e.currentTarget).closest("tr"));
+                        return navigate('.edit', {id: bank.id});
+                    }}], title: " ", width: "180px" }
+            ]
+        };
+    }
+}
