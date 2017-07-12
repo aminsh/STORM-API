543b5108bd60443a4aca69bcac9cdcea41aacbec
amin <aminsheikhi@gmail.com>
2017-07-13 01:24:14 +0430

    removed useless module

--- storm/client/src/app.js
+++ storm/client/src/app.js
@@ -8,8 +8,6 @@ import 'angular-ui-router';
 import 'angular-translate';
 import 'angular-cookies';
 
-import 'adm-dtp';
-
 import appRoute from './app.route';
 import shell from './layout/shell';
 import appTranslate from './app.translate';
@@ -22,45 +20,22 @@ import Api from './shared/api';
 import Logger from './shared/logger';
 import translate from './shared/translate';
 
-import HomeController from './home/home.controller';
-import AboutUSController from './ourTeam/aboutus.controller';
-import PricingController from './product/pricing.controller';
 
 import LoginController from './authentication/login.controller';
 import RegisterController from './authentication/register.controller';
 import UserApi from './authentication/api.user';
-
 import ProfileController from './profile/profile.controller';
-
-import ContactUsController from './contactUs/contactUs.controller';
-import RequestLucaDemoController from './product/requestLucaDemo.controller';
-
 import SetupInfoController from './branch/setup-info';
-import SetupFirstPeriodController from './branch/setup-firstPeriod';
-import SetupChartOfAccountsController from './branch/setup-chartOfAccounts';
 import BranchApi from './branch/api.branch';
-import FiscalPeriodApi from './branch/api.fiscalPeriod';
-import ChartOfAccounts from './branch/api.chartOfAccount';
-import uploader from './branch/logo.upload';
 
 angular.module('app', [
     'ngMessages',
     'ui.router',
     'pascalprecht.translate',
-    'ngCookies',
-    'ADM-dateTimePicker',
+    'ngCookies'
 ])
     .config(appRoute)
     .config(appTranslate)
-    .config(function (ADMdtpProvider) {
-        ADMdtpProvider.setOptions({
-            calType: 'jalali',
-            dtpType: 'date',
-            format: 'YYYY/MM/DD',
-            //default: 'today',
-            autoClose: true
-        });
-    })
 
     .run(authInit)
 
@@ -71,10 +46,6 @@ angular.module('app', [
     .service('Api', Api)
     .service('logger', Logger)
 
-    .controller('HomeController', HomeController)
-    .controller('HomeController', HomeController)
-    .controller('AboutUSController', AboutUSController)
-    .controller('PricingController', PricingController)
 
     .controller('LoginController', LoginController)
     .controller('RegisterController', RegisterController)
@@ -83,15 +54,8 @@ angular.module('app', [
     .controller('ProfileController', ProfileController)
 
     .controller('SetupInfoController', SetupInfoController)
-    .controller('SetupFirstPeriodController', SetupFirstPeriodController)
-    .controller('SetupChartOfAccountsController', SetupChartOfAccountsController)
-    .service('branchApi', BranchApi)
-    .service('fiscalPeriodApi', FiscalPeriodApi)
-    .service('chartOfAccountApi', ChartOfAccounts)
-    .directive('logoUploader', uploader)
 
-    .controller('ContactUsController', ContactUsController)
-    .controller('RequestLucaDemoController', RequestLucaDemoController);
+    .service('branchApi', BranchApi);
 
 
 export default angular.module('app');
