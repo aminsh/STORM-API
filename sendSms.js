708c14ae8ae12c4220f84637243eb22e7518d0de
amin <aminsheikhi@gmail.com>
2017-07-27 00:21:01 +0430

    added send sms test

---
+++ sendSms.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var Kavenegar = require('kavenegar');
+var api = Kavenegar.KavenegarApi({apikey: '31514D326B4F3668754D62384D7272536477315469413D3D'});
+api.Send({
+    message: "جناب آقای امین شیخی ثبت نام شما در نرم آنلاین استورم با موفقیت انجام شد",
+    sender: "100065995",
+    receptor: "09396408566"
+});
