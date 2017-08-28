97d40a8f0e2b0af5d39fbcfa8d41d85bc5603a2e
amin sheikhi <aminsheikhi@gmail.com>
2017-08-28 20:57:32 +0430

    normal changes

--- shared/utilities/httpException.js
+++ shared/utilities/httpException.js
@@ -2,6 +2,8 @@
 
 module.exports = class HttpException {
     constructor(statusCode, statusMessage, error) {
+        console.log(arguments);
+
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
         this.error = error ? JSON.parse(error) : null;
