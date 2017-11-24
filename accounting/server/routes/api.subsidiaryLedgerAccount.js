6423d7a419305ef2ec6af091c2f9bbb3248226e0
aminsh <@M!Nsh9710>
2017-11-24 19:36:24 +0330

    refactored api {generalLedgerAccount, subsidiaryLedgerAccount}

--- accounting/server/routes/api.subsidiaryLedgerAccount.js
+++ accounting/server/routes/api.subsidiaryLedgerAccount.js
@@ -2,8 +2,6 @@
 
 const async = require('asyncawait/async'),
     await = require('asyncawait/await'),
-    SubsidiaryLedgerAccountService = ApplicationService.SubsidiaryLedgerAccountService,
-    Guid = instanceOf('utility').Guid,
     EventEmitter = instanceOf('EventEmitter'),
     router = require('express').Router(),
     SubsidiaryLedgerAccountQuery = require('../queries/query.subsidiaryLedgerAccount');
