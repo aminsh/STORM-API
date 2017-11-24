933f582163a6f9c9acabe5f3b4254f5cc7eecd0e
aminsh <@M!Nsh9710>
2017-11-24 14:15:18 +0330

    added query service index to application

---
+++ application/queries/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports.DetailAccountQuery = require('../../accounting/server/queries/query.detailAccount');
+
+module.exports.ProductQuery = require('../../accounting/server/queries/query.product');
+module.exports.ProductCategoryQuery = require('../../accounting/server/queries/query.productCategory');
+module.exports.ScaleQuery = require('../../accounting/server/queries/query.scale');
+
+module.exports.SettingsQuery = require('../../accounting/server/queries/query.settings');
+module.exports.StockQuery = require('../../accounting/server/queries/query.stock');
+
+module.exports.PaymentQuery = require('../../accounting/server/queries/query.payment');
+
+module.exports.InvoiceQuery = require('../../accounting/server/queries/query.invoice');
+module.exports.InventoryQuery = require('../../accounting/server/queries/query.inventory');
+
+module.exports.FiscalPeriodQuery = require('../../accounting/server/queries/query.fiscalPeriod');
+module.exports.JournalQuery = require('../../accounting/server/queries/query.journal');
+module.exports.JournalGenerationTemplateQuery = require('../../accounting/server/queries/query.journalGenerationTemplate');
+module.exports.GenenralLedgerAccountQuery = require('../../accounting/server/queries/query.generalLedgerAccount');
+module.exports.SubsidiaryLedgerAccountQuery = require('../../accounting/server/queries/query.subsidiaryLedgerAccount');
+
+
