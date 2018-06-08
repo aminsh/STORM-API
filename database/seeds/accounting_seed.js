fb936af12158860653d97682c19d5928ff17c6f1
amin <aminsheikhi@gmail.com>
2018-06-08 15:55:06 +0430

    added storm order

--- database/seeds/accounting_seed.js
+++ database/seeds/accounting_seed.js
@@ -2,20 +2,47 @@
 
 const async = require('asyncawait/async'),
     await = require('asyncawait/await'),
-    enums = require('../../shared/enums');
+    enums = require('../../shared/enums'),
+    Uuid = require('uuid-token-generator'),
+    uuid = new Uuid,
+    idGenerate = () => uuid.generate();
 
 exports.seed = async(function (knex, Promise) {
 
-    const invoices = await(knex.select('*').from('invoices')
-        .where('invoiceType', 'purchase')
-        .where('invoiceStatus', '!=', 'draft')
-    );
+    let number = 10000,
+        trial = await(knex.select('id').from('storm_plans').where('category', 'Trial').first());
 
-    invoices.forEach(item => {
-        let updatedInvoice = {
-            invoiceStatus: 'confirmed'
-        };
+    if(!trial)
+        throw new Error('Trial is empty');
 
-        await(knex('invoices').where('id', item.id).update(updatedInvoice));
-    })
+    let branches = await(knex.select('*').from('branches').where('isUnlimited', false).orWhereNull('isUnlimited')),
+
+        orders = branches.map(item => ({
+            id: idGenerate(),
+            number: ++number,
+            issuedDate: item.createdAt,
+            branchId: item.id,
+            planId: trial.id,
+            duration: 1,
+            unitPrice: 0,
+            discount: 0
+        })),
+        subscriptions = branches.map(item => ({
+            startDate: item.createdAt,
+            endDate: nextMonth(item.createdAt),
+            planId: trial.id,
+            branchId: item.id,
+            isActiveApi: nextMonth(item.createdAt, 2) <= new Date,
+        }));
+
+    await(knex('storm_orders').insert(orders));
+    await(knex('branch_subscriptions').insert(subscriptions));
 });
+
+function nextMonth(date, value = 1) {
+    let endDate = new Date(date.valueOf());
+
+    endDate.setMonth(endDate.getMonth() + value);
+
+    return endDate;
+}
