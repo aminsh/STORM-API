97fa9a32cdef61eb6bcc487ac49fbe8e8bd6082d
amin <aminsheikhi@gmail.com>
2018-06-16 21:30:40 +0430

    added express utils

--- application/src/bootstrap.js
+++ application/src/bootstrap.js
@@ -1,5 +1,26 @@
 import "reflect-metadata";
-import "./di.config"
+import {container} from "./di.config"
 
 import "./StormOrder";
 import "./Branch";
+import "./User";
+
+import {Context} from "./Context";
+import {app} from "../../api/api.config";
+import {register} from "./core/expressUtlis";
+
+container.bind("Context").to(Context);
+
+app.use(function (req, res, next) {
+
+    req.container.bind('HttpContext').toConstantValue({request: req});
+
+    next();
+});
+
+register(app, container);
+
+
+
+
+
---
+++ application/src/core/expressUtlis.js
@@ -0,0 +1,130 @@
+import {injectable, decorate} from "inversify";
+import express from "express";
+import async from "asyncawait/async";
+
+let controllers = [],
+    methods = [];
+
+export function Controller(baseUrl, ...middleware) {
+    return function (target) {
+
+        controllers.push({name: target.name, baseUrl, middleware, target});
+
+        decorate(injectable(), target);
+    }
+}
+
+export function Get(url, ...middleware) {
+    return function (target, key) {
+
+        methods.push({
+            controllerName: target.constructor.name,
+            method: 'get',
+            url,
+            key,
+            middleware
+        });
+    }
+}
+
+export function Post(url, ...middleware) {
+    return function (target, key) {
+
+        methods.push({
+            controllerName: target.constructor.name,
+            method: 'post',
+            url,
+            key,
+            middleware
+        });
+    }
+}
+
+export function Put(url, ...middleware) {
+    return function (target, key) {
+
+        methods.push({
+            controllerName: target.constructor.name,
+            method: 'put',
+            url,
+            key,
+            middleware
+        });
+    }
+}
+
+export function Delete(url, ...middleware) {
+    return function (target, key) {
+
+        methods.push({
+            controllerName: target.constructor.name,
+            method: 'delete',
+            url,
+            key,
+            middleware
+        });
+    }
+}
+
+export function register(app = express(), container) {
+
+    controllers.forEach(ctrl => {
+
+        let router = express.Router(),
+
+            actions = methods.filter(m => m.controllerName === ctrl.name);
+
+        container.bind(ctrl.name).to(ctrl.target);
+
+        actions.forEach(action => {
+
+            action.middleware.forEach(key => _setMiddlewareForAction(router, action, key));
+
+            router.route(action.url)[action.method](async(function (req, res, next) {
+
+                let result = req.container.get(ctrl.name)[action.key](...arguments);
+
+                Promise.resolve(result)
+                    .then(value => {
+                        if (!res.headersSent) {
+
+                            if (typeof value !== 'undefined')
+                                res.send(value);
+                            else
+                                res.sendStatus(200);
+                        }
+                    })
+                    .catch(error => next(error));
+            }));
+
+        });
+
+        ctrl.middleware.forEach(key => app.use(ctrl.baseUrl, _setMiddlewareForController(ctrl.baseUrl, key, router)));
+        app.use(ctrl.baseUrl, router);
+
+    })
+}
+
+function _setMiddlewareForAction(router, action, key) {
+    router.route(action.url)[action.method](function (req, res, next) {
+
+        Promise.resolve(req.container.get(key).handler(...arguments))
+            .then(() => {
+
+            })
+            .catch(error => next(error));
+    })
+}
+
+function _setMiddlewareForController(baseUrl, key, router) {
+    router.use(baseUrl, function (req, res, next) {
+
+        Promise.resolve(req.container.get(key).handler(...arguments))
+            .then(() => {
+
+            })
+            .catch(error => next(error));
+    })
+}
+
+
