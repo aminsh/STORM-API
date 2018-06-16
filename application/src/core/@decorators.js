39004119b99a3090899f6bc1354327ff3fe8566d
amin <aminsheikhi@gmail.com>
2018-06-16 21:30:36 +0430

    added async and log decorators

--- application/src/core/@decorators.js
+++ application/src/core/@decorators.js
@@ -1,10 +1,10 @@
-import async from "asyncawait/async";
+import toAsync from "asyncawait/async";
 import {container} from "../di.config";
 
 export function eventHandler(eventName) {
     return function (target, key) {
 
-        EventEmitter.on(eventName, async(function () {
+        EventEmitter.on(eventName, toAsync(function () {
 
             let childContainer = container.createChild(),
                 state = arguments[0],
@@ -16,5 +16,28 @@ export function eventHandler(eventName) {
 
             instance[key](...restOfArguments);
         }));
+    }
+}
+
+export function async() {
+    return function (target, key, descriptor) {
+        descriptor.value = toAsync(descriptor.value);
+    }
+}
+
+export function log() {
+    return function (target, key, descriptor) {
+        let func = descriptor.value;
+
+        descriptor.value = function () {
+
+            console.log(arguments);
+
+            let result = func.apply(this, arguments);
+
+            console.log(result);
+
+            return result;
+        }
     }
 }
