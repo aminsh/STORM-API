c52ae7e35f33e4138b632028cfc43adb0a4518c5
amin <aminsheikhi@gmail.com>
2018-06-23 20:00:19 +0430

    normal changes

--- application/src/Branch/ShouldHaveBranchMiddleware.js
+++ application/src/Branch/ShouldHaveBranchMiddleware.js
@@ -36,7 +36,6 @@ export class ShouldHaveBranchMiddleware  {
         if(!result.canExecute)
             return ForbiddenResponseAction(result.message);
 
-
         req.user = this.userRepository.findOne({userId: member.userId});
         req.branchId = member.branchId;
 
