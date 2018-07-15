fac705616215b1234071e90bd0d6d1c148d268a6
amin <aminsheikhi@gmail.com>
2018-07-15 13:31:44 +0430

    bug fixed

--- application/src/domain/services/InvoicePurchaseDomainService.js
+++ application/src/domain/services/InvoicePurchaseDomainService.js
@@ -116,7 +116,8 @@ export class InvoicePurchaseDomainService {
                         quantity: line.quantity,
                         unitPrice: line.unitPrice,
                         discount: line.discount || 0,
-                        vat: line.vat || 0
+                        vat: line.vat || 0,
+                        tax: line.tax || 0
                     }
                 })
                 .toArray()
