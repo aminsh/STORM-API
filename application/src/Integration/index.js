3f962966e1d80e8f0007a3f731d435784dfe2d04
amin <aminsheikhi@gmail.com>
2018-08-22 13:50:10 +0430

    normal changes

--- application/src/Integration/index.js
+++ application/src/Integration/index.js
@@ -4,6 +4,9 @@ import {KaveNegarSmsService} from "./smsService/KaveNegar";
 
 export function register(container) {
 
+    container.bind("PaypingInterfacePaymentGateway").to(PaypingInterfacePaymentGateway);
+    container.bind("ZarinpalInterfacePaymentGateway").to(ZarinpalInterfacePaymentGateway);
+
     container.bind("Factory<ThirdParty>").toFactory(context => {
         return key => {
             if (key === 'payping')
