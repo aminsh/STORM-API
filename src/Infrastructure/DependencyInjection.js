1a0f2fb7c197d15a249e1d423730d46c9ad50c74
amin <aminsheikhi@gmail.com>
2018-08-23 23:13:27 +0430

    added EventBus to Dependency injection

--- src/Infrastructure/DependencyInjection.js
+++ src/Infrastructure/DependencyInjection.js
@@ -2,4 +2,8 @@ import {Container} from "inversify";
 
 const container = new Container({defaultScope: "Request"});
 
+import {EventBus} from "./EventBus";
+
+container.bind("EventBus").to(EventBus);
+
 export default container;
