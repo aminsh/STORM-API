f3b162e1933128183394b44aeb2358e5427ac03c
sanaz-slm <sanaz.slm1988@gmail.com>
2018-08-25 12:26:27 +0430

    add marketerId feild in invoices

---
+++ database/migrations/20180825121549_accounting_migrate_db.js
@@ -0,0 +1,18 @@
+exports.up = function (knex, Promise) {
+    return knex.schema
+        .table('invoices', table => {
+            table.string('marketerId');
+            table
+                .foreign('marketerId')
+                .references('id')
+                .inTable('detailAccounts')
+                .onDelete('SET NULL');
+        })
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .table('invoices', table => {
+            table.dropColumn('marketerId');
+        });
+};
