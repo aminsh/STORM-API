cb2aac24165cf53af735e6ef5731efc32ec49df2
amins <aminsheikhi@gmail.com>
2019-01-26 22:53:40 +0330

    normal changes

--- src/Branch/ShouldHaveBranchForGuestUserMiddleware.js
+++ src/Branch/ShouldHaveBranchForGuestUserMiddleware.js
@@ -2,7 +2,7 @@ import {inject, injectable} from "inversify";
 import {async} from "../Infrastructure/@decorators";
 
 @injectable()
-export class ShouldHaveBranchForGuestUserMiddleware  {
+export class ShouldHaveBranchForGuestUserMiddleware {
 
     @inject("BranchRepository")
     /** @type {BranchRepository}*/ branchRepository = undefined;
@@ -23,15 +23,15 @@ export class ShouldHaveBranchForGuestUserMiddleware  {
 
         const branchId = req.query.branchId;
 
-        if(!branchId)
-            return  ForbiddenResponseAction();
+        if (!branchId)
+            return ForbiddenResponseAction();
 
         const branch = this.branchRepository.findById(branchId);
 
-        if(!branch)
+        if (!branch)
             return ForbiddenResponseAction();
 
-        req.user = {};
+        req.user = req.user || {};
         req.branchId = branchId;
 
         this.fiscalPeriodMiddlewareSelector.handle(req);
