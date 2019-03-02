6730c6039250647a3843831cfed15131904f38e3
amins <aminsheikhi@gmail.com>
2019-03-02 13:07:40 +0330

    bug fixed

--- src/JournalGeneration/index.js
+++ src/JournalGeneration/index.js
@@ -1,13 +1,8 @@
 import {JournalSaleGenerationService} from "./JournalSaleGenerationService";
 import {JournalSaleEventListener} from "./JournalSaleEventListener";
-import {JournalPurchaseGenerationService} from "./JournalPurchaseGenerationService";
-import {JournalPurchaseEventListener} from "./JournalPurchaseEventListener";
 
 export function register(container) {
 
     container.bind("JournalSaleGenerationService").to(JournalSaleGenerationService);
     container.bind("JournalSaleEventListener").to(JournalSaleEventListener);
-
-    /*container.bind("JournalPurchaseGenerationService").to(JournalPurchaseGenerationService);
-    container.bind("JournalPurchaseEventListener").to(JournalPurchaseEventListener);*/
 }
