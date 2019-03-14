c0f58f608f056c941e5e212809851e353f357218
amins <aminsheikhi@gmail.com>
2019-03-14 16:55:47 +0330

    journal generated automatically when purchase created applied

--- src/JournalGeneration/index.js
+++ src/JournalGeneration/index.js
@@ -1,8 +1,11 @@
-import {JournalSaleGenerationService} from "./JournalSaleGenerationService";
-import {JournalSaleEventListener} from "./JournalSaleEventListener";
+import { JournalSaleGenerationService } from "./JournalSaleGenerationService";
+import { JournalSaleEventListener } from "./JournalSaleEventListener";
+import { JournalPurchaseEventListener } from "./JournalPurchaseEventListener";
 
 export function register(container) {
 
     container.bind("JournalSaleGenerationService").to(JournalSaleGenerationService);
     container.bind("JournalSaleEventListener").to(JournalSaleEventListener);
+
+    container.bind("JournalPurchaseEventListener").to(JournalPurchaseEventListener);
 }
